2c759b3be9eef83fde24030f99e4a7f1cacb6fd0
Ryan Zegray <ryan.zegray@gmail.com>
2020-04-03 14:50:59 -0400

    Firebase auth

--- src/App.js
+++ src/App.js
@@ -1,16 +1,42 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import withFirebaseAuth from "react-with-firebase-auth";
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from "./config/firebase";
 
-function App() {
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const firebaseAppAuth = firebaseApp.auth();
+
+const providers = {
+  // googleProvider: new firebase.auth.GoogleAuthProvider(),
+  facebookProvider: new firebase.auth.FacebookAuthProvider(),
+};
+
+function App(props) {
+  const { user, signOut, signInWithGoogle, signInWithFacebook } = props;
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello World!</p>
+        {user ? <p>Hello, {user.displayName}</p> : <p>Please sign in.</p>}
+        {user ? (
+          <button onClick={signOut}>Sign out</button>
+        ) : (
+          <div>
+            {/* <button onClick={signInWithGoogle}>Sign in with Google</button> */}
+            <button onClick={signInWithFacebook}>Sign in with FB</button>
+          </div>
+        )}
       </header>
     </div>
   );
 }
 
-export default App;
+export default withFirebaseAuth({
+  providers,
+  firebaseAppAuth,
+})(App);
